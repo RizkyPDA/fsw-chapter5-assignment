80c60190cab294e38cfb6f9e16977fbd8b843067
RizkyPDA <muhammad.syadrie11@gmail.com>
2022-01-21 16:21:02 +0700

    tambah modul add

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const fs = require("fs"); // Import modul File System
 const express = require("express"); // Import modul Express.js
 const app = express();
+const fs = require("fs"); // Import modul File System
 
 app.set("view engine", "ejs"); // Setting view engine
 app.set("views", __dirname + "/public/views"); // Setting Lokasi views
@@ -18,6 +18,26 @@ app.get("/", (req, res) => {
     pageTitle: "Main",
     data: dataParse,
   });
+});
+
+app.get("/add", (req, res) => {
+  res.render("add", {
+    pageTitle: "Add",
+  });
+});
+
+app.post("/add", (req, res) => {
+  const { nama, email, password } = req.body;
+  const data = fs.readFileSync("./data/data.json", "utf-8");
+  const dataParsed = JSON.parse(data);
+  const newUser = {
+    nama,
+    email,
+    password,
+  };
+  dataParsed.push(newUser);
+  fs.writeFileSync("./data/data.json", JSON.stringify(dataParsed, null, 4));
+  res.redirect("/");
 });
 
 const PORT = 3000;
